af0685cac5d410ae8a4339e9af58f792a8f7d8b1
Dheeraj Pal <dheerajpal6018@gmail.com>
2026-05-31 22:10:16 +0530

    1

---
+++ App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+import Student from './Student'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h2>Student Information</h2>
+
+      <Student name="Aniket" course="B.Tech" marks="9" />
+      <Student name="Amit" course="B.Tech" marks="09" />
+      <Student name="Amrit" course="B.Tech" marks="78" />
+    </div>
+  )
+}
+
+export default App
